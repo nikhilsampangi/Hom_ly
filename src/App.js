26b2cf1c09f4228bb9f7f5a3052acea84dc620a1
nikhilsampangi <nikhil.sampangi@gmail.com>
2020-02-25 14:38:41 +0530

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,80 +1,79 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Component, Fragment } from "react";
 import "./App.css";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      greeting: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+import FrontPage from "./front_page/FrontPage";
+import Cust from "./customer/Cust";
+import Chef from "./chef/Chef";
+import Staff from "./transport/Staff";
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       greeting: ""
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
+//   handleChange(event) {
+//     this.setState({ name: event.target.value });
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+//       .then(response => response.json())
+//       .then(state => this.setState(state));
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <form onSubmit={this.handleSubmit}>
+//             <label htmlFor="name">Enter your name: </label>
+//             <input
+//               id="name"
+//               type="text"
+//               value={this.state.name}
+//               onChange={this.handleChange}
+//             />
+//             <button type="submit">Submit</button>
+//           </form>
+//           <p>{this.state.greeting}</p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path="/" component={FrontPage} />
+            <Route exact path="/Home" component={Cust} />
+            <Route exact path="/Chef" component={Chef} />
+            <Route exact path="/Transport" component={Staff} />
+          </Switch>
+        </Fragment>
+      </Router>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
