50805058622909750dc0653ee9075587005492fe
nikhilsampangi <nikhil.sampangi@gmail.com>
2020-02-27 03:10:24 +0530

    mergin

--- src/front_page/FrontPage.js
+++ src/front_page/FrontPage.js
@@ -2,10 +2,6 @@ import React, { Component, Fragment } from "react";
 
 export default class FrontPage extends Component {
   render() {
-    return (
-      <Fragment>
-        <p>Sup nigga 3</p>
-      </Fragment>
-    );
+    return <Fragment></Fragment>;
   }
 }
