d8f86ce6f9b53eb585d1e80f7a38d5edcefb30bd
nikhilsampangi <nikhil.sampangi@gmail.com>
2020-02-26 19:46:58 +0530

    db connected user model strtd

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
-
+const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
+
+mongoose.connect("mongodb://localhost:27017/ciodb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection
+  .once("open", function() {
+    console.log("db connected");
+  })
+  .on("error", function() {
+    console.log("err: ", error);
+  });
 
 // app.get("/api/greeting", (req, res) => {
 //   const name = req.query.name || "World";
---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: [true, "Username cannot be empty"] },
+
+  hashedPassword: {
+    type: String,
+    required: [true, "Password cannot be empty"]
+  },
+
+  phone: {
+    type: Number,
+    required: [true, "Phone number cannot be empty"],
+
+    validate: {
+      validator: function(v) {
+        var re = /^\d{10}$/;
+        return re.test(v);
+      },
+      message: "Phone number must be 10 digit number"
+    }
+  },
+
+  email: {
+    type: String,
+    required: [true, "email cannot be empty"],
+
+    validate: {
+      validator: function(v) {
+        var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return re.test(v);
+      },
+      message: "Please fill a valid email address"
+    }
+  },
+
+  isVerified: { type: Boolean, default: false },
+
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Prefer not to say"],
+    default: "Prefer not to say"
+  }
+});
+
+schema.set("toJSON", { virtuals: true });
+
+module.exports = mongoose.model("customers", UserSchema);
